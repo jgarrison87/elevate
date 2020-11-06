845916f09ce1816fc0202a46c121ca03e2ddecf8
jgarrison87 <64297933+jgarrison87@users.noreply.github.com>
2020-11-06 17:36:51 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -17,3 +17,30 @@ function modalOpen5() {
 }
 
 setTimeout("modalOpen5()", 5000);
+
+// lightbox
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+
+previews.forEach((preview) => {
+  preview.addEventListener("click", () => {
+    modal.classList.add("open");
+    original.classList.add("open");
+
+    // dynamic change text and image
+    const originalSrc = preview.getAttribute("data-original");
+    original.src = `../img/home/${originalSrc}`;
+    const altText = preview.alt;
+    caption.innerText = altText;
+  });
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
