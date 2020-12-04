981c95282238cd0f41a8eb2c9b3d6863c9eeb8be
jgarrison87 <64297933+jgarrison87@users.noreply.github.com>
2020-12-04 11:40:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-const toggle = document.getElementById("toggle");
-const close = document.getElementById("close");
-const open = document.getElementById("open");
-const modal = document.getElementById("modal");
-
-// hamburger menu
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
+const modalClose = document.getElementById('modal-close');
 
+// hamburger menu
 hamburger.addEventListener('click', () => {
   navUL.classList.toggle('show');
 })
 
-// Toggle nav
-toggle.addEventListener("click", () =>
-  document.body.classList.toggle("show-nav")
-);
 
-// Show Modal
-open.addEventListener("click", () => modal.classList.add("modal-active"));
 
-//Hide Modal
-close.addEventListener("click", () => modal.classList.remove("modal-active"));
+// email modal
+function modalOpen5() {
+  document.querySelector(".modal-bg").style.display = 'block'
+};
 
-//Hide Modal on outside click
-window.addEventListener("click", (e) =>
-  e.target == modal ? modal.classList.remove("modal-active") : false
-);
+setTimeout("modalOpen5()", 5000);
+
+// email modal close
+modalClose.addEventListener('click', () => {
+  document.querySelector(".modal-bg").style.display = 'none'
+});
