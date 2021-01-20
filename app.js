ee083416f9e77accd788bc9229ffd2154ba69bef
jgarrison87 <64297933+jgarrison87@users.noreply.github.com>
2021-01-19 20:58:31 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 const modalClose = document.getElementById('modal-close');
+const goals = document.getElementById('goals');
+const dropdown= document.getElementById('dropdown')
+
+// goals menu
+goals.addEventListener('click', () => {
+  dropdown.classList.toggle('show');
+})
 
 // hamburger menu
 hamburger.addEventListener('click', () => {
   navUL.classList.toggle('show');
 })
-
-
 
 // email modal
 function modalOpen5() {
--- js/health.js
+++ js/health.js
@@ -1,6 +1,13 @@
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 const modalClose = document.getElementById('modal-close');
+const goals = document.getElementById('goals');
+const dropdown= document.getElementById('dropdown')
+
+// goals menu
+goals.addEventListener('click', () => {
+  dropdown.classList.toggle('show');
+})
 
 // hamburger menu
 
